85b63df5c0c04410fd29acd19009027aa8f66a0a
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-08-25 17:38:44 +0600

    API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -6,24 +7,75 @@ function App() {
     { name: "Illustrator", price: "$890.99" },
     { name: "Pdf Reader", price: "$780.99" },
   ];
+  const productNames = products.map((product) => product.name);
+  console.log(productNames);
   return (
     <div className="App">
       <header className="App-header">
-        <Product product={products[0]}></Product>
-        <Product product={products[1]}></Product>
-        <Product product={products[2]}></Product>
+        <Counter></Counter>
+        <User></User>
+        <ul>
+          {products.map((product) => (
+            <li>{product.name}</li>
+          ))}
+        </ul>
+        {products.map((product) => (
+          <Product product={product}></Product>
+        ))}
       </header>
     </div>
   );
 }
+
+// count starts
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => setCount(count + 1);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={handleIncrease}>Increase</button>
+    </div>
+  );
+}
+//count ends
+
+
+/***** User Starts From Here *****/
+function User(){
+  const [users,setUsers] = useState([0]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>{
+      setUsers(data);
+    })
+  },[])
+  return(
+    <div>
+      <h3>Dynamic User</h3>
+      <ul>
+        {
+          users.map(user=><li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+/***** User Ends From Here *****/
+
+
+
 function Product(props) {
   const productStyles = {
     border: "1px solid gray",
     borderRadius: "5px",
-    backgroundColor: "lightgray",
+    backgroundColor: "black",
     height: "200px",
     width: "200px",
     float: "left",
+    color: "green",
   };
   return (
     <div style={productStyles}>
