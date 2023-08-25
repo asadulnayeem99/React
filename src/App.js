763cdffeab7c2682e38cc6a53671d15611a4b56b
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-08-25 12:55:21 +0600

    New

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const products = [
+    { name: "Photoshop", price: "$90.99" },
+    { name: "Illustrator", price: "$890.99" },
+    { name: "Pdf Reader", price: "$780.99" },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
       </header>
+    </div>
+  );
+}
+function Product(props) {
+  const productStyles = {
+    border: "1px solid gray",
+    borderRadius: "5px",
+    backgroundColor: "lightgray",
+    height: "200px",
+    width: "200px",
+    float: "left",
+  };
+  return (
+    <div style={productStyles}>
+      <h2>{props.product.name}</h2>
+      <h2>{props.product.price}</h2>
+      <button>Buy</button>
     </div>
   );
 }
